f8889d668dd9b773ead1aec23fe256b071c7478c
Naima Jabbar <naimajabbar909@gmail.com>
2026-03-16 23:31:07 +0500

    Add task manager service

---
+++ src/services/task-manager.ts
@@ -0,0 +1,38 @@
+import type { Task } from '../models/tasks';
+
+abstract class TaskManager {
+    abstract addTask(id: number, title: string, description: string, status: 'pending' | 'in-progress' | 'completed'): void;
+
+    abstract getTask(id: number): Task | undefined;
+
+    abstract updateTask(id: number, title?: string, description?: string, status?: 'pending' | 'in-progress' | 'completed'): void;
+
+    abstract deleteTask(id: number): void;
+}
+
+class InmemoryTaskManager extends TaskManager {
+    private tasks: Task[] = [];
+
+    addTask(id: number, title: string, description: string, status: 'pending' | 'in-progress' | 'completed'): void {
+        const task: Task = { id, title, description, status };
+        this.tasks.push(task);
+    }
+
+    getTask(id: number): Task | undefined {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    updateTask(id: number, title?: string, description?: string, status?: 'pending' | 'in-progress' | 'completed'): void {
+        const taskIndex = this.tasks.findIndex(task => task.id === id);
+        if (taskIndex !== -1) {
+            const task = this.tasks[taskIndex]!;
+            if (title !== undefined) task.title = title;
+            if (description !== undefined) task.description = description;
+            if (status !== undefined) task.status = status;
+        }
+    }
+
+    deleteTask(id: number): void {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+}
